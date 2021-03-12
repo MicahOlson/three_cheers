64af2a2a2527840c609d285a0ae222bad33078f2
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-12 09:18:29 -0800

    Add #delete test and code to pass

--- lib/project.rb
+++ lib/project.rb
@@ -47,4 +47,8 @@ class Project
     @title = attributes.fetch(:title)
     DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
   end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  end
 end
