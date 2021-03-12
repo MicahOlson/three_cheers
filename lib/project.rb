8fa38ac9c8c9fedbf9d500b238f1c618b23a2804
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-12 09:19:59 -0800

    Refactor accessor methods

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,10 @@
 class Project
+  attr_reader :id
+  attr_accessor :title
+
   def initialize(attributes)
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
-  end
-
-  def title
-    @title
-  end
-
-  def id
-    @id
   end
 
   def ==(project_to_compare)
