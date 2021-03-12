8388e6c42515d262261ec191ed35bcffce10c10b
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-12 09:12:39 -0800

    Add #update test and code to pass

--- lib/project.rb
+++ lib/project.rb
@@ -42,4 +42,9 @@ class Project
       nil
     end
   end
+
+  def update(attributes)
+    @title = attributes.fetch(:title)
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  end
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -46,4 +46,13 @@ describe Project do
       expect(Project.find(project1.id)).to eq project1
     end
   end
+
+  describe '#update' do
+    it 'allows a user to update a project' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      project.save
+      project.update({:title => 'Teaching Ruby to Kids', :id => nil})
+      expect(project.title).to eq 'Teaching Ruby to Kids'
+    end
+  end
 end
