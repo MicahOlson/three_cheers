637fc7d3c7e54f43aa5124e79289834faeb381a0
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-03-12 09:26:46 -0800

    Refactor accessor methods

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,15 +1,9 @@
 class Volunteer
+  attr_reader :name, :project_id
+
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
-  end
-
-  def name
-    @name
-  end
-
-  def project_id
-    @project_id
   end
 end
